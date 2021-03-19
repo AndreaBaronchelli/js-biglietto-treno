93f07ddec5d266233b4992c0d7aac4574902f17a
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-03-19 14:50:34 +0100

    some style and output

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ TASKS
     1 - ask km numbers
     2 - ask utent age
     3 - calc price (0.21€/Km discount for <18 and >65) 
+    4 - output
 */
 
 //1
@@ -16,6 +17,8 @@ var km = parseInt( prompt("How many km do you have to do?") );
 if ( isNaN(km) ) {
     alert("Invalid format. The text must be a number. Please refresh page.");
 }
+
+document.getElementById("km").innerHTML = km + ("Km");
 
 //console.log(km);
 
@@ -35,11 +38,21 @@ var price = km * 0.21;
 
 var discount;
 if (age < 18) {
-    discount = (price * 20) / 100
+    discount = (price * 20) / 100;
+    document.getElementById("discount").innerHTML = "20%";
+
 } else if (age >= 65) {
-    discount = (price * 40) / 100
+    discount = (price * 40) / 100;
+    document.getElementById("discount").innerHTML = "40%";
+} else {
+    discount = 0
+    document.getElementById("discount").innerHTML = "0%";
 }
 //console.log(discount);
 
 var finalPrice = (price - discount).toFixed(2);
 //console.log(finalPrice);
+
+//4
+
+document.getElementById("finalprice").innerHTML = finalPrice + "€";
